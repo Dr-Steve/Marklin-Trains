382ced29aa30a8900159d5f07d7aba10b82d4990
Steve <steve.northup@gmail.com>
2012-03-26 20:56:37 -0400

    Real Initial Commit -Steve

---
+++ build/web/jsp/index.jsp
@@ -0,0 +1,16 @@
+<%@ page contentType="text/html; charset=UTF-8" %>
+
+<%@ taglib prefix="s" uri="/struts-tags" %>
+
+<html>
+    <head>
+        <title><s:text name="Marklin Trains"/></title>
+    </head>
+
+    <body>
+        <div id="banner">
+            <h1>Marklin Trains</h1>
+        </div>
+    </body>
+</html>
+
---
+++ src/java/api/marklinAPI.java
@@ -0,0 +1,19 @@
+
+package api;
+
+/**
+ *
+ * @author steve
+ */
+public class marklinAPI {
+  
+  /*
+   * TODO: Add any methods and implimentations needed to connect to the train.
+   *        These should include such things as emergencyStop(); etc.
+   *        Someone should set up stubs for all the methods we need including
+   *        parameters and descriptions of what they will do, then people
+   *        can claim them and implement without overlap.
+   * 
+   */
+  
+}
